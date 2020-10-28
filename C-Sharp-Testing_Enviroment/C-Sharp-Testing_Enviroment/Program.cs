4447e58473a0c8180a5baf0bb7210a27b6de0ebc
SpaceminerJB <72396639+SpaceminerJB@users.noreply.github.com>
2020-10-28 21:47:19 +0100

    Projekt erstellt
    Projekt in C# erstellt und für Testzwecke erst mal hochgeladen

---
+++ C-Sharp-Testing_Enviroment/C-Sharp-Testing_Enviroment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace C_Sharp_Testing_Enviroment
+{
+    class Program
+    {
+        static void Main()
+        {
+            string alter;
+
+            Console.Write("Bitte gib dein Alter ein: ");
+            alter = Console.ReadLine();
+            Console.WriteLine("Du bist " + alter + " Jahre alt.");
+            Console.ReadKey();
+
+
+     
+        
+        }
+
+        
+
+    }
+
+
+}
